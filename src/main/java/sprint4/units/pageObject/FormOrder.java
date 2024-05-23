06f33d78e5a4da2f60ad1133a06453ca50a9ee5e
Alexander Sostskov <sotskovalexander@yandex.ru>
2024-05-23 15:31:39 +0300

    third version progect

--- src/main/java/sprint4/units/pageObject/FormOrder.java
+++ src/main/java/sprint4/units/pageObject/FormOrder.java
@@ -1,4 +1,4 @@
-package sprint4.units;
+package sprint4.units.pageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,29 +10,43 @@ import java.time.temporal.ChronoUnit;
 
 public class FormOrder {
     private final WebDriver webDriver;
+    //Адрес главной страницы
     private static final String URL = "https://qa-scooter.praktikum-services.ru/";
+    //Локатор кнопки с подтверждением использования куки
     private final By cookie = By.xpath(".//button[@class='App_CookieButton__3cvqF']");
+    //Кнопка заказать сверху страницы
     private final By orderButton = By.xpath(".//button[@class='Button_Button__ra12g']");
+    //Кнопка заказать внизу страницы
+    private final By orderButtonBottom = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    //Поле ввода имени
     private final By inputName = By.xpath(".//input[@placeholder='* Имя']");
-    private static String name = "Саша";
+    //Поле ввода фамилии
     private final By inputLastName = By.xpath(".//input[@placeholder='* Фамилия']");
-    private static String lastName = "Иванов";
+    //Поле ввода адреса
     private final By inputAddres = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
-    private static String addres = "Москва";
+    //Поле ввода станции метро
     private final By inputSubwayStation = By.xpath(".//input[@placeholder='* Станция метро']");
-    private static String subwayStation = "Черкизовская";
-    private final By choiceSubwayStation = By.xpath(".//div[text()='Черкизовская']");
+    //Локатор выбора станции метро
+    private final By choiceSubwayStation = By.xpath(".//div[@class='select-search__select'][1]");
+    //Поле ввода номера телефона
     private final By inputPhoneNumber = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
-    private static String phonenumber = "+79805614360";
+    //Локатор кнопки далее
     private final By buttonNext = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    //Локатор даты доставки
     private final By inputDate = By.xpath(".//div[@aria-label='Choose понедельник, 10-е июня 2024 г.']");
+    //Поле ввода даты доставки
     private final By clickDate = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
-    private static String date = "10.06.2024";
+    //Поле выбора периода аренды
     private final By inputPeriod = By.xpath(".//div[@class='Dropdown-placeholder']");
+    //Локкатор выбора периода аренды
     private final By choicePeriod = By.xpath(".//div[text()='сутки']");
+    //Локатор выбора цвета
     private final By choiceColor = By.xpath(".//input[@id='black']");
+    //Локатор кнопка "Заказать" в конце формы заказа
     private final By orderFinalButton = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    //Локатор кнопки "Да" во всплывающем окне
     private final By yesButton = By.xpath(".//button[text()='Да']");
+    //Локатор текста "Заказ оформлен" при усппешном заказе
     private final By orderSuccess = By.xpath(".//div[text()='Заказ оформлен']");
 
 
@@ -55,28 +69,28 @@ public class FormOrder {
         return this;
     }
 
-    public FormOrder enterName() {
+    public FormOrder enterName(String name) {
         webDriver.findElement(inputName).sendKeys(name);
         return this;
     }
 
-    public FormOrder enterLastName() {
+    public FormOrder enterLastName(String lastName) {
         webDriver.findElement(inputLastName).sendKeys(lastName);
         return this;
     }
 
-    public FormOrder enterAddres() {
+    public FormOrder enterAddres(String addres) {
         webDriver.findElement(inputAddres).sendKeys(addres);
         return this;
     }
 
-    public FormOrder enterSubwayStation() {
+    public FormOrder enterSubwayStation(String subwayStation) {
         webDriver.findElement(inputSubwayStation).sendKeys(subwayStation);
         webDriver.findElement(choiceSubwayStation).click();
         return this;
     }
 
-    public FormOrder enterPhoneNumber() {
+    public FormOrder enterPhoneNumber(String phonenumber) {
         webDriver.findElement(inputPhoneNumber).sendKeys(phonenumber);
         return this;
     }
@@ -86,7 +100,7 @@ public class FormOrder {
         return this;
     }
 
-    public FormOrder clickDate() {
+    public FormOrder clickDate(String date) {
         webDriver.findElement(clickDate).click();
         webDriver.findElement(clickDate).sendKeys(date);
         return this;
@@ -116,6 +130,11 @@ public class FormOrder {
 
     public FormOrder clickYes() {
         webDriver.findElement(yesButton).click();
+        return this;
+    }
+
+    public FormOrder clickOrderButtonBottom() {
+        webDriver.findElement(orderButtonBottom).click();
         return this;
     }
 
--- src/main/java/sprint4/units/pageObject/Questions.java
+++ src/main/java/sprint4/units/pageObject/Questions.java
@@ -1,4 +1,4 @@
-package sprint4.units;
+package sprint4.units.pageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -8,13 +8,8 @@ import org.openqa.selenium.WebElement;
 
 public class Questions {
     private final WebDriver webDriver;
-
+    //Ссылка на главную страницу
     private static final String URL = "https://qa-scooter.praktikum-services.ru/";
-
-    //private final By question = By.xpath(".//div[text()='Сколько это стоит? И как оплатить?']");
-    //private final By answer = By.xpath(".//p[text()='Сутки — 400 рублей. Оплата курьеру — наличными или картой.']");
-
-
     public Questions(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
